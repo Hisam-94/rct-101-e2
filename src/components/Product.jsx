3be51fe407e2cd53cb88e4b07be15affe516fa92
Hisam-94 <hisamuddin94@gmail.com>
2022-05-30 16:19:44 +0530

    React - Eval - 2

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,40 @@
 import React from "react";
-
+import { Text } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
+import { Center, Square, Circle } from '@chakra-ui/react'
+import {
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  TagRightIcon,
+  TagCloseButton,
+} from '@chakra-ui/react'
 const Product = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+  // const Text = () => <div />;
+  // const Image = () => <div />;
+  // const Box = () => <div />;
+  // const Stack = () => <div />;
+  // const Heading = () => <div />;
+  // const Tag = () => <div />;
+  // const TagLabel = () => <div />;
   return (
-    <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
-      <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
+    <Stack data-cy="product" direction={['column']} spacing='10px' boxShadow='lg' p='6' rounded='md' bg='white'>
+      <Image data-cy="product-image" src='https://bit.ly/dan-abramov' alt='Dan Abramov'borderRadius='10px'/>
+      <Flex justifyContent="space-between">
+      <Text data-cy="product-category" fontSize='2xl'>Jeans</Text>
+      <Tag size={{10:"px"}} key={1} variant='solid' colorScheme='teal'>
+        <TagLabel data-cy="product-gender">Unisex</TagLabel>
       </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
+      </Flex>
+      
+      <Heading data-cy="product-title" as='h3' size='lg'>React Tshirt</Heading>
+      <Box data-cy="product-price" w='100%' p={4} color='black'>Rs.300/Unit</Box>
+      
     </Stack>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Addproduct from "./AddProduct"
+import Product from "./Product";
+import Pagination from "./Pagination"
+import { Flex, Spacer } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
-
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+  
   return (
+    <>
     <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+    {/*  AddProduct */}
+      <Addproduct/>
+      <Grid><Product/></Grid>
+      <Pagination/>
       {/* Pagination */}
     </Flex>
+    </>
   );
 };
 
